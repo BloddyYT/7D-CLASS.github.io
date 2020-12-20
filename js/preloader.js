0b799c1da52a5eb55e253e77b801a940f1679d57
Linux-Penguin-ai <you@example.com>
2020-12-20 18:54:29 +0400

    test

---
+++ js/preloader.js
@@ -0,0 +1,10 @@
+info = ['Դասղեկն է Լուսինե Բաղդասարյանը','Դասարանում կա 21 տղա 9 աղջիկ','Կայքը ստեղծվել է 5 օրում','Շնորհավոր Ամանօր և Սուրբ Ծնունդ','Դասարանում կա 30 աշակերտ','Դասարանում կա 6/7 գերազանցիկ']
+var random = Math.floor(Math.random() * info.length);
+$('#hi').text((info[random]));
+
+  window.onload = (function () {
+  	setTimeout(function(){
+  		$('.preloader').fadeOut();
+
+  	},1250);
+  });
