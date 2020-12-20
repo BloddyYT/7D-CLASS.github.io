b518f38743b3a9dbe1925991b7eff8f975a204c6
Linux-Penguin-ai <you@example.com>
2020-12-20 22:11:47 +0400

    УМРИТЕ БАГИ

--- js/preloader.js
+++ js/preloader.js
@@ -1,12 +1,12 @@
-info = ['Դասղեկն է Լուսինե Բաղդասարյանը','Դասարանում կա 21 տղա 9 աղջիկ','Կայքը ստեղծվել է 5 օրում','Շնորհավոր Ամանօր և Սուրբ Ծնունդ','Դասարանում կա 30 աշակերտ','Դասարանում կա 6/7 գերազանցիկ']
+$('#pr').css('background', '#858585');
+info = ['Շնորհավոր Ամանօր և Սուրբ Ծնունդ']
 var random = Math.floor(Math.random() * info.length);
 $('#hi').text((info[random]));
 if(info[random] == 'Շնորհավոր Ամանօր և Սուրբ Ծնունդ'){
 	$(".preloader__item").css('backgroundColor', '#fff');
-	$('#pr').removeClass('preloader')
-	$('#pr').addClass('preloader2')
+	$('#pr').css("background", "url('file:///C:/Users/MM/Documents/7d/img/wlp.jpg')");
 	
-}
+}//Hello World :)
 
   window.onload = (function () {
   	setTimeout(function(){
